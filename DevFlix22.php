99138c1bd5189993d6989ff92b20f11f4475cf64
KevenSoares22 <keven.v.soares@gmail.com>
2022-05-11 16:37:44 -0300

    DeveloperFlix

---
+++ DevFlix22.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+
+<html lang="pt-br">
+
+<head>
+
+<meta charset="UTF-8">
+<title>DevFlix</title>
+<link rel="stylesheet" href="DevFlix22.css">
+        
+		
+
+
+
+</head>
+
+<body>
+
+<div class="flexbx">
+<header>
+<nav>
+   
+
+     <h1>DEVELOPERFLIX</h1>
+	
+	
+	
+	
+	<div class="mobile">
+	<div class="linha01"></div>
+	<div class="linha02"></div>
+	<div class="linha03"></div>
+	</div>
+      <ul class="nav-list">
+	  
+	  
+
+        <li><a href="DevFlix22.php">Cadastrar</a></li>
+        <li><a href="DevFlix22.php">Login</a></li>
+		<li><a href="DevFlix22.php">Filmes de Comédia</a></li>
+		
+		<li><a href="DevFlix22.php">Filmes de Ação</a></li>
+		
+      </ul>
+
+ 
+
+ 
+ </nav>
+</header> 
+
+
+ <main class="cards">
+        <section class="card 01">
+		
+		
+            <div class="icon">
+                <img src="https://upload.wikimedia.org/wikipedia/pt/1/1a/O_Jogo_da_Imita%C3%A7%C3%A3o.jpg" alt="Assistir">
+            </div>
+            <h3>O jogo da imitação</h3>
+            <span>Resumo.</span>
+            <a href="DevFlix2222.php" ><button>Assistir agora</button></a>
+        </section>
+        <section class="card 02">
+            <div class="icon">
+                <img src="https://br.web.img2.acsta.net/medias/nmedia/18/87/15/18/20028633.jpg" alt="Assistir">
+            </div>
+            <h3>Uma mente brilhante</h3>
+            <span>Resumo.</span>
+            <a href="DevFlix2222.php" ><button>Assistir agora</button></a>
+        </section>
+        <section class="card 03">
+            <div class="icon">
+                <img src="https://media.fstatic.com/gCdO8en9W76DwhtJXNCnWnXCq3M=/290x478/smart/media/movies/covers/2013/11/a-rede-social_t14928_3.jpg" alt="Assistir">
+            </div>
+            <h3>A rede social</h3>
+            <span>Resumo.</span>
+            <a href="DevFlix2222.php"><button>Assistir agora</button></a>
+        
+	
+		
+		
+		
+        </section>
+
+		
+		
+		
+		</section>
+		
+
+    </main>
+	</div>
+	
+	
+  
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+
+
+  
+
+
+<script src="DevFlix22.js"></script>
+
+</body>
+
+</html>
